69bd6a93de7c8d84158ec88ff111945f6d9ee4f8
LazaProMaster <143183202+LazaProMaster@users.noreply.github.com>
2024-07-30 13:14:45 +0200

    Create MeniBeta.py

---
+++ meni/MeniBeta.py
@@ -0,0 +1,97 @@
+import pygame as pg
+
+pg.init()
+screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+##screen = pg.display.set_mode((500, 400))
+clock = pg.time.Clock()
+running = True
+font = pg.font.Font("freesansbold.ttf", 100)
+text = font.render('pipacu te', True, "green", "blue")
+
+class MainMenu:
+    def __init__(self):
+        self.W = screen.get_size()[0]
+        self.H = screen.get_size()[1]
+    def DrawMainMenu(self):
+        mpos = pg.mouse.get_pos()
+        ##Tilte
+        textS = font.render('The Game', True, "Black")
+        RectS = textS.get_rect()
+        RectS.center = (960, 100)
+        screen.blit(textS, RectS)
+
+        ##settings
+        pg.draw.rect(screen, "black", (30, 200, 500, 780), 10, 10)
+        pg.draw.rect(screen, "black", (30, 200, 500, 150), 0, 10)
+        textS = font.render('Settings', True, "white")
+        RectS = textS.get_rect()
+        RectS.center = (280, 275)
+        screen.blit(textS, RectS)
+
+        ##leaderboard
+        pg.draw.rect(screen, "black", (1390, 200, 500, 780), 10)
+        pg.draw.rect(screen, "black", (1390, 200, 500, 150))
+        textW = font.render('Most win', True, "white")
+        RectW = textW.get_rect()
+        RectW.center = (1640, 275)
+        screen.blit(textW, RectW)
+
+        ##play, singleplayer, credits, quit game
+        if (mpos[0] >= 560 and mpos[0] <= 1360 and mpos[1] <= 390 and mpos[1] >= 250):
+            pg.draw.rect(screen, "gray", (560, 250, 800, 140), 0, 25)
+        else:
+            pg.draw.rect(screen, "black", (560, 250, 800, 140), 0, 25)
+        textP = font.render('Play', True, "white")
+        RectP = textP.get_rect()
+        RectP.center = (960, 320)
+        screen.blit(textP, RectP)
+
+        if (mpos[0] >= 560 and mpos[0] <= 1360 and mpos[1] <= 560 and mpos[1] >= 420):
+            pg.draw.rect(screen, "gray", (560, 420, 800, 140))
+        else:
+            pg.draw.rect(screen, "black", (560, 420, 800, 140))
+        textI = font.render('Singleplayer', True, "white")
+        RectI = textI.get_rect()
+        RectI.center = (960, 490)
+        screen.blit(textI, RectI)
+
+        if (mpos[0] >= 560 and mpos[0] <= 1360 and mpos[1] <= 730 and mpos[1] >= 590):
+            pg.draw.rect(screen, "gray", (560, 590, 800, 140))
+        else:
+            pg.draw.rect(screen, "black", (560, 590, 800, 140))
+        textC = font.render('Credits', True, "white")
+        RectC = textC.get_rect()
+        RectC.center = (960, 660)
+        screen.blit(textC, RectC)
+
+        if (mpos[0] >= 560 and mpos[0] <= 1360 and mpos[1] <= 900 and mpos[1] >= 760):
+            pg.draw.rect(screen, "gray", (560, 760, 800, 140))
+        else:
+            pg.draw.rect(screen, "black", (560, 760, 800, 140))
+        textQ = font.render('Quit Game', True, "white")
+        RectQ = textQ.get_rect()
+        RectQ.center = (960, 830)
+        screen.blit(textQ, RectQ)
+
+
+
+MainMenu = MainMenu()
+
+print(screen.get_size())
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    screen.fill("white")
+    
+    MainMenu.DrawMainMenu()
+    ##screen.blit(text, (0, 0, 200, 200))
+
+    ##print(pg.mouse.get_pressed())
+
+    pg.display.flip()
+
+    clock.tick(60)
+
+pg.quit()
